e99284cb477fed6c06be3f320c66be434d17434b
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2024-05-09 15:06:09 +0200

    header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Animals from './pages/Animals'
-import FAQ from './pages/FAQ'
-import AboutUs from './pages/AboutUs'
-import Donation from './pages/Donate'
-import Contact from './pages/Contact'
+import { Suspense, lazy } from 'react'
+import { AppBar } from './components/AppBar/AppBar';
 
-
+const HomePage = lazy(() => import('./pages/HomePage'));
+const Animals = lazy(() => import('./pages/Animals'));
+const FAQ = lazy(() => import('./pages/FAQ'));
+const AboutUs = lazy(() => import('./pages/AboutUs'));
+const Donation = lazy(() => import('./pages/Donate'));
+const Contact = lazy(() => import('./pages/Contact'));
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <div>
+      <AppBar/>
+      <Suspense fallback={<div>Loading Page...</div>}>
       <Routes>
           <Route path='/' element={<HomePage/>} />
           <Route path='/animalslist' element={<Animals/>} />
@@ -19,8 +22,9 @@ const App = () => {
           <Route path='/about' element={<AboutUs />} />
           <Route path='/donation' element={<Donation/>} />
           <Route path='/contacts' element={<Contact/>} />
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </Suspense>
+    </div>
   )
 }
 
---
+++ vite-project/src/components/AppBar/AppBar.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+const AppBar = () => {
+    return (
+        <header>
+            <nav>
+                <NavLink to='/' className={buildLinkClass}>
+                    Home Page
+                </NavLink>
+                <NavLink to='/aboutUs' className={buildLinkClass}>
+                    About Us
+                </NavLink>
+                <NavLink to='/animals' className={buildLinkClass}>
+                    Animals
+                </NavLink>
+                <NavLink to='/donation' className={buildLinkClass}>
+                    Donate
+                </NavLink>
+                <NavLink to='/contactUs' className={buildLinkClass}>
+                    Contact
+                </NavLink>
+                <NavLink to='/faq' className={buildLinkClass}>
+                    FAQ
+                </NavLink>
+            </nav>
+    </header>
+)
+}
+export default AppBar;
